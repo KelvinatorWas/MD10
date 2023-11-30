b1373c71d915b043a5d577d3f8adafed4a5311a2
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-11-30 22:19:09 +0200

    MD10

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-alert */
 import $ from 'jquery';
 import axios from 'axios';
@@ -7,12 +8,22 @@ import { validateFormInput } from './utils/validateFormInputs/validateFormInputs
 // console.log('Body jQuery node:', $('body'));
 // console.log('Body javascript node:', document.querySelector('body'));
 
+const sectionContainer = $('section');
+
 type User = {
   id?: number;
   name:string;
   email:string;
   password:string;
 }
+
+const createElement = (type = 'div', className = '', id = '') => {
+  const element = $(`<${type}>`);
+  if (className.length) element.addClass(className);
+  if (id.length) element.attr('id', id);
+
+  return element;
+};
 
 const pushData = (data:User) => {
   try {
@@ -34,8 +45,66 @@ const lookUpEmail = async (email:string) => {
   return false;
 };
 
+const createInput = (parent: JQuery<HTMLElement>, text: string, type = '', name = '', ph = '') => {
+  const bsRow = createElement('div', 'row');
+  const inputLabel = createElement('label', 'custom-label').attr('for', name).text(text);
+  const bsColumn = createElement('div', 'col-12');
+
+  const inputArea = createElement('input', 'custom-input-box', name);
+  inputArea.attr('type', type);
+  inputArea.attr('name', name);
+  inputArea.attr('placeholder', ph);
+  inputArea.attr('required');
+
+  bsColumn.append(inputArea);
+  bsRow.append(inputLabel);
+  bsRow.append(bsColumn);
+
+  parent.append(bsRow);
+};
+
+const generateForm = () => {
+  const createRegBtn = (parent: JQuery<HTMLElement>) => {
+    const bsRow = createElement('div', 'row');
+    const bsColumn = createElement('div', 'col-12');
+    const inputButton = createElement('input', 'btn-input').attr('type', 'submit');
+    inputButton.attr('value', 'Register');
+
+    bsColumn.append(inputButton);
+    bsRow.append(bsColumn);
+    parent.append(bsRow);
+  };
+
+  const createAllInputs = (parent: JQuery<HTMLElement>) => {
+    const inputFrame = createElement('div', 'input-f');
+
+    createInput(inputFrame, 'Email', 'text', 'email', 'example@prov.com');
+    createInput(inputFrame, 'Name', 'text', 'name', 'John Doe');
+    createInput(inputFrame, 'Password', 'password', 'password', '••••••••');
+    createInput(inputFrame, 'Re-enter Password', 'password', 'repassword', '••••••••');
+
+    parent.append(inputFrame);
+  };
+
+  const inputDiv = createElement('div', 'input-frame');
+  const formContiner = createElement('form', undefined, 'register');
+  const container = createElement('div', 'container');
+
+  // input frame, where it contains input Elements;
+  createAllInputs(container);
+
+  // button
+  createRegBtn(container);
+
+  formContiner.append(container);
+  inputDiv.append(formContiner);
+  sectionContainer.append(inputDiv);
+};
+
+generateForm();
+
 $(() => {
-  $('#log-in').on('submit', async (event) => {
+  $('#register').on('submit', async (event) => {
     event.preventDefault();
 
     const [em, na, pas, rpas] = [$('#email').val(), $('#name').val(), $('#password').val(), $('#repassword').val()] as string[];
--- src/utils/apiService/apiService.test.ts
+++ src/utils/apiService/apiService.test.ts
@@ -4,11 +4,15 @@ const axios = require('axios');
 
 jest.mock('axios');
 
-test('response', async () => {
-  expect.assertions(1);
-  const user = { name: 'John Doe', email: 'testing@verycool.com', password: 'myNam3jeff2012!', id: 0 };
-  const payload = { data: user };
+describe('Test Axios', () => {
+  it('can recive data', async () => {
+    expect.assertions(1);
+    const user = {
+      name: 'John Doe', email: 'testing@verycool.com', password: 'myNam3jeff2012!', id: 0,
+    };
+    const payload = { data: user };
 
-  axios.get = jest.fn().mockResolvedValue(payload);
-  await expect(fetchData()).resolves.toEqual(user);
+    axios.get = jest.fn().mockResolvedValue(payload);
+    await expect(fetchData()).resolves.toEqual(user);
+  });
 });
